f9e1a647c279f7e7603d1f9c23845d66dce16e9a
boydzweers <boydzweers46@gmail.com>
2021-05-20 20:41:42 +0200

    jest coverage settings

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,14 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['bin'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
 };
